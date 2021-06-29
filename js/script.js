93de8890515ad9098d3564edb820604281576001
adinapop <adinampop@yahoo.com>
2021-06-29 15:46:29 +0200

    Function addTask done & working

--- js/script.js
+++ js/script.js
@@ -4,11 +4,20 @@ new Vue (
     {
         el: "#app",
         data: {
-
+            toDoList: [
+                "Fare i compiti",
+                "Fare la spesa",
+                "Fare il bucato"
+            ],
+            newTaskToDo: ""
         },
 
         methods: {
-            
+        addTask: function () {
+            this.toDoList.push(this.newTaskToDo);
+            // aggiungendo dopo una stringa vuota ci elimina quello scritto precedentemnte e lascia spazio nuovo per scrivere
+            this.newTaskToDo = "";
+        }
         }
     }
 )
