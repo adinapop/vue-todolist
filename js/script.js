90effbc4534efe5102f5cda1adb995723af24b57
adinapop <adinampop@yahoo.com>
2021-06-29 16:24:01 +0200

    Added autofocus

--- js/script.js
+++ js/script.js
@@ -12,16 +12,31 @@ new Vue (
             newTaskToDo: ""
         },
 
+        // legge sia il JS che l'HTML
+        mounted() {
+            // giusto per provare, faccio partire l'autofocus dopo 1s
+            setTimeout(() => {
+                this.autofocusFunction();
+            }, 1000);
+        },
+
         methods: {
+            autofocusFunction: function() {
+                const focusElement = document.getElementById("autofocus-input");
+                focusElement.focus();
+            },
+
             addTask: function () {
                 this.toDoList.push(this.newTaskToDo);
                 // aggiungendo dopo una stringa vuota ci elimina quello scritto precedentemnte e lascia spazio nuovo per scrivere
                 this.newTaskToDo = "";
+                this.autofocusFunction();
             },
             
             deleteTask: function(index) {
                 //da questa lista togli un elemento a partire dall'index = che è lo start
                 this.toDoList.splice(index, 1);
+                this.autofocusFunction();
             }
         }
     }
